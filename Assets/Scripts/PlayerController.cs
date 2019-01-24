ec6b4a99617f6a0a510abdf3ce10af4ef67ea331
JesseHamel <47010711+JesseHamel@users.noreply.github.com>
2019-01-24 23:30:35 +0000

    Second Commit
    Added Jumping and death animations.
    Added enemy spawner.
    Added score counter.
    Added destroyer to delete enemies when they collide with it.
    Changed the PlayerCharacter sprite to better suit in-game animations.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,24 +1,76 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     private Rigidbody2D myRigidbody;
-    public float PlayerJumpForce = 500f;
+    private Animator myAnim;
+    public float PlayerJumpForce = 600f;
+    private float PlayerDeathTime = -1;
+    private Collider2D myCollider;
+    public Text ScoreText;
+    private float StartTime;
 
 	// Use this for initialization
 	void Start () {
         myRigidbody = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
+        myCollider = GetComponent<Collider2D>();
+
+        StartTime = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetButtonDown("Jump"))
+        if (PlayerDeathTime == -1)
         {
-            myRigidbody.AddForce(transform.up * PlayerJumpForce);
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                myRigidbody.AddForce(transform.up * PlayerJumpForce);
+            }
+
+            myAnim.SetFloat("vVelocity", myRigidbody.velocity.y);
+
+            ScoreText.text = (Time.time - StartTime).ToString("0.0");
+        }
+        else
+        {
+
+            if(Time.time > PlayerDeathTime + 2)
+            {
+
+                Application.LoadLevel(Application.loadedLevel);
+
+            }
+            
         }
 
-	}
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+
+            foreach (PrefabSpawner spawner in FindObjectsOfType<PrefabSpawner>())
+            {
+                spawner.enabled = false;
+            }
+
+            foreach (MoveLeft moveLefter in FindObjectsOfType<MoveLeft>())
+            {
+                moveLefter.enabled = false;
+            }
+
+            PlayerDeathTime = Time.time;
+            myAnim.SetBool("PlayerDeath", true);
+            myRigidbody.velocity = Vector2.zero;
+            myRigidbody.AddForce(transform.up * PlayerJumpForce);
+            myCollider.enabled = false;
+        }
+    }
 }
---
+++ Assets/Scripts/PrefabSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabSpawner : MonoBehaviour {
+
+    private float NextSpawn = 0;
+    public Transform PrefabToSpawn;
+    public float SpawnRate = 1;
+    public float RandomDelay = 1;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Time.time > NextSpawn)
+        {
+            Instantiate(PrefabToSpawn, transform.position, Quaternion.identity);
+            NextSpawn = Time.time + SpawnRate + Random.Range(0, RandomDelay);
+        }
+
+	}
+}
